64f4eabb4570d817b9c704a08373d58020bf9908
khanhhx <hoangxuankhanh.work@gmail.com>
2025-11-08 16:53:26 +0700

    feat(post): update post reaction listeners to handle breakdown counts

--- social-network-be/src/use-case/post/listeners/listeners.service.ts
+++ social-network-be/src/use-case/post/listeners/listeners.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { PostRepository } from 'src/domains/post/post.repository';
-import { ReactionEvents } from 'src/share/events';
+import { ReactionCreatedEventPayload, ReactionEvents } from 'src/share/events';
 
 @Injectable()
 export class ListenersService {
   constructor(private readonly postRepository: PostRepository) {}
   private readonly logger = new Logger(ListenersService.name);
   @OnEvent(ReactionEvents.created)
-  async handleReactionCreated(payload: any): Promise<void> {
+  async handleReactionCreated(
+    payload: ReactionCreatedEventPayload,
+  ): Promise<void> {
     try {
-      await this.postRepository.increaseReactionCount(payload.targetId);
+      await this.postRepository.increaseReactionCount(
+        payload.targetId,
+        payload.reactionType,
+      );
     } catch (error) {
       this.logger.error(
         `Failed to increment reaction count for post ${payload.targetId}`,
@@ -19,10 +24,29 @@ export class ListenersService {
     }
   }
 
+  @OnEvent(ReactionEvents.updated)
+  async handleReactionUpdated(payload: any): Promise<void> {
+    try {
+      await this.postRepository.updateReactionCount(
+        payload.targetId,
+        payload.oldReactionType,
+        payload.newReactionType,
+      );
+    } catch (error) {
+      this.logger.error(
+        `Failed to update reaction count for post ${payload.targetId}`,
+        error.stack,
+      );
+    }
+  }
+
   @OnEvent(ReactionEvents.removed)
   async handleReactionRemoved(payload: any): Promise<void> {
     try {
-      await this.postRepository.decreaseReactionCount(payload.targetId);
+      await this.postRepository.decreaseReactionCount(
+        payload.targetId,
+        payload.reactionType,
+      );
     } catch (error) {
       this.logger.error(
         `Failed to decrement reaction count for post ${payload.targetId}`,
