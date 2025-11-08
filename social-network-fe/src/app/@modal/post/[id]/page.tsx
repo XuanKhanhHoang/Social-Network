832ca03ed48420827a35109e12a52a9ece433f76
khanhhx <hoangxuankhanh.work@gmail.com>
2025-11-08 15:03:37 +0700

    feat: Refactor post detail component

--- social-network-fe/src/app/@modal/post/[id]/page.tsx
+++ social-network-fe/src/app/@modal/post/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-import PostDetail from '@/components/features/Post/detail/PostDetail';
+import PostDetail from '@/components/features/Post/detail/Main';
 import { usePost } from '@/hooks/post/usePost';
+import { transformToPostWithMyReaction } from '@/lib/interfaces/post';
 import { useParams, useSearchParams } from 'next/navigation';
 
 export default function PostDetailPage() {
@@ -9,9 +10,10 @@ export default function PostDetailPage() {
   const id = params.id;
   const mediaIndex = Number(searchParams.get('m') ?? 0);
   const { data: post, isPending, isError } = usePost(id);
+  console.log(post?.media);
   return (
     <PostDetail
-      post={post}
+      post={post ? transformToPostWithMyReaction(post) : undefined}
       initialMediaIndex={mediaIndex}
       isError={isError}
       isPending={isPending}
