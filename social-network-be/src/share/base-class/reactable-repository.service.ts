e8fff9dcaf426288c47f28b11fce1a00b3822663
khanhhx <hoangxuankhanh.work@gmail.com>
2025-11-08 16:53:26 +0700

    feat(reactable): implement reaction breakdown counts in ReactableRepository

--- social-network-be/src/share/base-class/reactable-repository.service.ts
+++ social-network-be/src/share/base-class/reactable-repository.service.ts
@@ -1,20 +1,70 @@
 import { ClientSession, Document } from 'mongoose';
 import { BaseRepository } from './base-repository.service';
+import { ReactionType } from '../enums';
 
 export abstract class ReactableRepository<
   T extends Document,
 > extends BaseRepository<T> {
   async increaseReactionCount(
     id: string,
+    reactionType: ReactionType,
     session?: ClientSession,
   ): Promise<T | null> {
-    return this.updateByIdAndGet(id, { $inc: { reactionsCount: 1 } }, session);
+    return this.updateByIdAndGet(
+      id,
+      {
+        $inc: {
+          reactionsCount: 1,
+          [`reactionsBreakdown.${reactionType}`]: 1,
+        },
+      },
+      session,
+    );
   }
+  async updateReactionCount(
+    id: string,
+    oldReactionType: ReactionType,
+    newReactionType: ReactionType,
+    session?: ClientSession,
+  ): Promise<T | null> {
+    const oldReactionFieldKey = `reactionsBreakdown.${oldReactionType}`;
+    const oldReactionFieldRef = `$${oldReactionFieldKey}`;
+    const newReactionFieldKey = `reactionsBreakdown.${newReactionType}`;
+    const newReactionFieldRef = `$${newReactionFieldKey}`;
+    const updatePipeline = [
+      {
+        $set: {
+          [oldReactionFieldKey]: {
+            $max: [0, { $subtract: [oldReactionFieldRef, 1] }],
+          },
+          [newReactionFieldKey]: {
+            $add: [newReactionFieldRef, 1],
+          },
+        },
+      },
+    ];
 
+    return this.updateByIdAndGet(id, updatePipeline, { session });
+  }
   async decreaseReactionCount(
     id: string,
+    reactionType: ReactionType,
     session?: ClientSession,
   ): Promise<T | null> {
-    return this.updateByIdAndGet(id, { $inc: { reactionsCount: -1 } }, session);
+    const reactionFieldKey = `reactionsBreakdown.${reactionType}`;
+    const reactionFieldRef = `$${reactionFieldKey}`;
+    const updatePipeline = [
+      {
+        $set: {
+          reactionsCount: {
+            $max: [0, { $subtract: ['$reactionsCount', 1] }],
+          },
+          [reactionFieldKey]: {
+            $max: [0, { $subtract: [reactionFieldRef, 1] }],
+          },
+        },
+      },
+    ];
+    return this.updateByIdAndGet(id, updatePipeline, { session });
   }
 }
